ef32f1ece91dad4b17ecf7946a69046141840581
John Reg <johnreginaldjao@gmail.com>
2024-01-31 23:02:21 +0800

    created menu for UpdateFragment

--- app/src/main/java/com/johnreg/to_doapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/johnreg/to_doapp/fragments/update/UpdateFragment.kt
@@ -3,18 +3,47 @@ package com.johnreg.to_doapp.fragments.update
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.johnreg.to_doapp.R
+import com.johnreg.to_doapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+
+    private lateinit var binding: FragmentUpdateBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setMenu()
+    }
+
+    private fun setMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.update_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
