71cf53ef82a575cb924a1646f949897c86574e2c
John Reg <johnreginaldjao@gmail.com>
2024-02-01 16:41:51 +0800

    created ToDoRepository

---
+++ app/src/main/java/com/johnreg/to_doapp/data/ToDoRepository.kt
@@ -0,0 +1,13 @@
+package com.johnreg.to_doapp.data
+
+import androidx.lifecycle.LiveData
+
+class ToDoRepository(private val toDoDao: ToDoDao) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData) {
+        toDoDao.insertData(toDoData)
+    }
+
+}
