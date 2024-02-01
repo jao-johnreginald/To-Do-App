b76dcaccf73c1bd5ee5820705eae6524eebcd249
John Reg <johnreginaldjao@gmail.com>
2024-02-01 11:27:24 +0800

    created Entity ToDoData

---
+++ app/src/main/java/com/johnreg/to_doapp/data/ToDoData.kt
@@ -0,0 +1,13 @@
+package com.johnreg.to_doapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String
+)
