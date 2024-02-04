b59c172c3dde633ddbf590ab4f6601843d66de51
John Reg <johnreginaldjao@gmail.com>
2024-02-04 20:43:20 +0800

    insert data to database when menu_add clicked

--- app/src/main/java/com/johnreg/to_doapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/johnreg/to_doapp/fragments/add/AddFragment.kt
@@ -1,7 +1,6 @@
 package com.johnreg.to_doapp.fragments.add
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -10,13 +9,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.johnreg.to_doapp.R
+import com.johnreg.to_doapp.data.models.Priority
+import com.johnreg.to_doapp.data.models.ToDoData
+import com.johnreg.to_doapp.data.viewmodel.ToDoViewModel
 import com.johnreg.to_doapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
     private lateinit var binding: FragmentAddBinding
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,9 +49,44 @@ class AddFragment : Fragment() {
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                return false
+                return when (menuItem.itemId) {
+                    R.id.menu_add -> {
+                        insertData()
+                        true
+                    }
+                    else -> false
+                }
             }
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun insertData() {
+        when {
+            binding.etTitle.text.isEmpty() -> makeSnackbar("Please add a title.")
+            else -> {
+                val newData = ToDoData(
+                    title = binding.etTitle.text.toString(),
+                    description = binding.etDescription.text.toString(),
+                    priority = parsePriority(binding.spinner.selectedItem.toString())
+                )
+                mToDoViewModel.insertData(newData)
+                makeSnackbar("Successfully added!")
+                // Navigate back
+                findNavController().navigate(R.id.action_addFragment_to_listFragment)
+            }
+        }
+    }
+
+    private fun makeSnackbar(text: String) {
+        Snackbar.make(binding.root, text, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun parsePriority(priority: String): Priority {
+        return when (priority) {
+            "Medium Priority" -> Priority.MEDIUM
+            "Low Priority" -> Priority.LOW
+            else -> Priority.HIGH
+        }
     }
 
 }
