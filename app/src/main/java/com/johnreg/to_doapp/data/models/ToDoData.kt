3efc8b7d8474e66b04ddf795fd346bced4f8af60
John Reg <johnreginaldjao@gmail.com>
2024-02-04 19:49:24 +0800

    added default values in Entity

--- app/src/main/java/com/johnreg/to_doapp/data/models/ToDoData.kt
+++ app/src/main/java/com/johnreg/to_doapp/data/models/ToDoData.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "todo_table")
 data class ToDoData(
     @PrimaryKey(autoGenerate = true)
-    var id: Int,
+    var id: Int = 0,
     var title: String,
-    var priority: Priority,
-    var description: String
+    var priority: Priority = Priority.HIGH,
+    var description: String? = null
 )
