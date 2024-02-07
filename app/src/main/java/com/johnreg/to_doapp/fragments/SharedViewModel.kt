fece8902185b3ba651c924e02b7627a654f77622
John Reg <johnreginaldjao@gmail.com>
2024-02-07 20:54:03 +0800

    transferred function to SharedViewModel

--- app/src/main/java/com/johnreg/to_doapp/fragments/SharedViewModel.kt
+++ app/src/main/java/com/johnreg/to_doapp/fragments/SharedViewModel.kt
@@ -39,4 +39,10 @@ class SharedViewModel(application: Application): AndroidViewModel(application) {
         }
     }
 
+    fun parsePriorityToInt(priority: Priority) = when (priority) {
+        Priority.HIGH -> 0
+        Priority.MEDIUM -> 1
+        Priority.LOW -> 2
+    }
+
 }
--- app/src/main/java/com/johnreg/to_doapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/johnreg/to_doapp/fragments/update/UpdateFragment.kt
@@ -1,7 +1,6 @@
 package com.johnreg.to_doapp.fragments.update
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -10,11 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.navArgs
 import com.johnreg.to_doapp.R
-import com.johnreg.to_doapp.data.models.Priority
 import com.johnreg.to_doapp.databinding.FragmentUpdateBinding
 import com.johnreg.to_doapp.fragments.SharedViewModel
 
@@ -58,14 +57,8 @@ class UpdateFragment : Fragment() {
     private fun setUI() {
         binding.etTitle.setText(args.currentItem.title)
         binding.etDescription.setText(args.currentItem.description)
-        binding.spinner.setSelection(parsePriority(args.currentItem.priority))
+        binding.spinner.setSelection(mSharedViewModel.parsePriorityToInt(args.currentItem.priority))
         binding.spinner.onItemSelectedListener = mSharedViewModel.listener
-    }
-
-    private fun parsePriority(priority: Priority) = when (priority) {
-        Priority.HIGH -> 0
-        Priority.MEDIUM -> 1
-        Priority.LOW -> 2
     }
 
 }
