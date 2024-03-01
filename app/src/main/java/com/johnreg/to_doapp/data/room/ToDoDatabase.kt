e466c95cb4260f9514feec4762cce610d5c61762
John Reg <johnreginaldjao@gmail.com>
2024-03-01 21:22:29 +0800

    refactored ToDoDatabase - changed if to when

--- app/src/main/java/com/johnreg/to_doapp/data/room/ToDoDatabase.kt
+++ app/src/main/java/com/johnreg/to_doapp/data/room/ToDoDatabase.kt
@@ -11,11 +11,20 @@ import com.johnreg.to_doapp.data.models.ToDoData
 @TypeConverters(Converter::class)
 abstract class ToDoDatabase: RoomDatabase() {
 
+    /*
+    The method has no body or argument because Room database is going to be handling it
+    The database exposes DAOs through an abstract getter method for each DAO operation
+     */
     abstract fun toDoDao(): ToDoDao
 
+    // Singleton
     companion object {
 
-        // Volatile - writes to this field are immediately made visible to other threads
+        /*
+        Volatile - writes to this field are immediately made visible to other threads
+        No matter what thread is running this code, don't store this variable in any thread cache
+        I want you to store this centrally in main memory
+         */
         @Volatile
         private var INSTANCE: ToDoDatabase? = null
 
@@ -24,29 +33,27 @@ abstract class ToDoDatabase: RoomDatabase() {
         If INSTANCE is not null, then return that same INSTANCE
         If INSTANCE is null, then inside a synchronized block create an instance of our database class
 
-        We are using synchronized because we don't want multiple threads to create multiple instances
-        With the synchronized block, we make sure that only 1 thread is able to access this code
-        inside our synchronized block, and only 1 thread will be able to create an instance of this class
+        synchronized - do not allow this block of code to run on multiple threads
+        If more than one thread tries to create an instance of the database at the same time,
+        it will be blocked, it allows creation of only one instance at a time
+
+        fallbackToDestructiveMigration() - whenever you change the Entity and increase the database version,
+        this function will delete all data, call this if you know you may be changing your database schema
          */
 
         fun getDatabase(context: Context): ToDoDatabase {
             val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            /*
-            When a thread calls synchronized, it acquires the lock of that synchronized block
-            Other threads don't have permission to call that same synchronized block
-            as long as previous thread which had acquired the lock does not release the lock
-             */
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ToDoDatabase::class.java,
-                    "todo_database"
-                ).fallbackToDestructiveMigration().build()
-                INSTANCE = instance
-                return instance
+            return when {
+                tempInstance != null -> tempInstance
+                else -> synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ToDoDatabase::class.java,
+                        "todo_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                    instance
+                }
             }
         }
 
