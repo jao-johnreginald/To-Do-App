9a5b931e87f5904417e8297a1ef1d13a2be6a534
John Reg <johnreginaldjao@gmail.com>
2024-03-26 11:57:40 +0800

    refactored SharedViewModel - clean architecture | added comments | rearranged code

--- app/src/main/java/com/johnreg/to_doapp/ui/sharedviewmodel/SharedViewModel.kt
+++ app/src/main/java/com/johnreg/to_doapp/ui/sharedviewmodel/SharedViewModel.kt
@@ -7,6 +7,7 @@ import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.johnreg.to_doapp.R
 import com.johnreg.to_doapp.data.models.Priority
@@ -14,22 +15,29 @@ import com.johnreg.to_doapp.data.models.ToDoData
 
 class SharedViewModel(application: Application) : AndroidViewModel(application) {
 
-    val spinnerListener: OnItemSelectedListener = object : OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            when (position) {
-                0 -> (parent?.getChildAt(0) as? TextView)?.setTextColor(ContextCompat.getColor(application, R.color.red))
-                1 -> (parent?.getChildAt(0) as? TextView)?.setTextColor(ContextCompat.getColor(application, R.color.yellow))
-                2 -> (parent?.getChildAt(0) as? TextView)?.setTextColor(ContextCompat.getColor(application, R.color.green))
-            }
-        }
-        override fun onNothingSelected(parent: AdapterView<*>?) {}
-    }
+    /** ============================= List Fragment ============================= */
 
     // Creates a MutableLiveData initialized with the given value
-    val isDatabaseEmpty: MutableLiveData<Boolean> = MutableLiveData(false)
+    private val _isDatabaseEmpty: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isDatabaseEmpty: LiveData<Boolean> get() = _isDatabaseEmpty
 
     fun setMutableLiveData(dataList: List<ToDoData>) {
-        isDatabaseEmpty.value = dataList.isEmpty()
+        _isDatabaseEmpty.value = dataList.isEmpty()
+    }
+
+    /** ============================= Add/Update Fragment ============================= */
+
+    val spinnerListener: OnItemSelectedListener = object : OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val textOfSpinner = parent?.getChildAt(0) as? TextView
+            val color = when (position) {
+                1 -> ContextCompat.getColor(application, R.color.yellow)
+                2 -> ContextCompat.getColor(application, R.color.green)
+                else -> ContextCompat.getColor(application, R.color.red)
+            }
+            textOfSpinner?.setTextColor(color)
+        }
+        override fun onNothingSelected(parent: AdapterView<*>?) {}
     }
 
     fun parseStringToPriority(string: String): Priority = when (string) {
