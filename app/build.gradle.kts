6c966140cee6d5bedbfefde396c66d60102f369e
John Reg <johnreginaldjao@gmail.com>
2024-08-09 18:41:25 +0800

    upgraded dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,12 +47,12 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Navigation component
     val navVersion = "2.7.7"
@@ -67,7 +67,7 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
 
     // Convert Flow to LiveData with .asLiveData()
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
 
     // RecyclerView Animator
     implementation("jp.wasabeef:recyclerview-animators:4.0.2")
